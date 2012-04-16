91a7cd1f2aee883ca193726bccae14acffa0f3a4
heraldmatias <heraldmatias.oz@gmail.com>
2012-04-16 01:48:14 -0500

    Update src/apps/alumno/urls.py

--- src/apps/alumno/urls.py
+++ src/apps/alumno/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('alumno.views',
     url(r'^guardar/$', 'alumno', name='alumno-listar'),
     url(r'^registrar/$','alumno', name='alumno-registrar'),
+    url(r'^historial/$','alumno_historial', name='alumno-historial'),
+    url(r'^historial/(?P<alumno>\d+)-(?P<tipo>\d+)/ajax/$','alumno_historial_ajax', name='alumno-historial-ajax'),
 )
